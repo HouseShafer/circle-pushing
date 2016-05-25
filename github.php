1c65f4cc3fb3d063218c977f530ce5bfca794a77
Danny Shafer <daniel.shafer@jewsforjesus.org>
2016-05-25 13:22:54 -0700

    do the whole git sequence on the server side

--- github.php
+++ github.php
@@ -4,10 +4,10 @@
  </head>
  <body>
  <?php echo '<p>Hello World</p>'; ?> 
+ <?php `git pull github test`; ?>
  <?php `git checkout test`; ?>
- <?php echo '<p>Hello World</p>'; ?> 
- <?php echo '<p>Hello World</p>'; ?> 
- <?php echo '<p>Hello World</p>'; ?> 
+ <?php `git pull github master`; ?>
+ <?php `git checkout master`; ?>
  <?php echo '<p>Hello World</p>'; ?> 
  </body>
 </html>
