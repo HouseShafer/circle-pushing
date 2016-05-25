dc1268e9e53fd8adc3975f45dfdf5de04f0ef9f2
Danny Shafer <daniel.shafer@jewsforjesus.org>
2016-05-25 14:02:27 -0700

    add git stash

--- github.php
+++ github.php
@@ -4,12 +4,13 @@
  </head>
  <body>
  <?php echo '<p>Hello World</p>'; ?> 
+ <?php `git stash`; ?>
  <?php `git pull github test`; ?>
  <?php `git checkout test`; ?>
  <?php `git pull github master`; ?>
  <?php `git checkout master`; ?>
  <?php echo '<p>Hello World</p>'; ?> 
- <?php echo '<h5>did it work!?</h5>'; ?> 
+ <?php echo '<h1>did it work!?</h1>'; ?> 
  </body>
 </html>
 
